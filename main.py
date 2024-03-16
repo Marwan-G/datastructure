891376467fbc3675a80c60e9b3bf918981c95ac1
Marwan-G <ghabin2004@gmail.com>
2024-03-16 20:45:12 +0000

    add append to Linkedlist

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ class LinkedList:
     while temp is not None:
       print(temp.value)
       temp=temp.next
+  def append(self,value):
+     new_node = Node(value)
+     if self.head is None:
+       self.head = new_node
+       self.tail = new_node
+     else:
+       self.tail.next = new_node
+       self.tail= new_node
+       self.length +=1
+#    return True
